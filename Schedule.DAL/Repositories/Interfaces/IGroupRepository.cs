769471c84bb9e30c7ac1719f582b3066bdc5be18
nsskvkn <d9127506@gmail.com>
2026-05-13 20:12:01 +0300

    feat(dal): add IRepository, interfaces, GenericRepository, IUnitOfWork

---
+++ Schedule.DAL/Repositories/Interfaces/IGroupRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.DAL.Repositories.Interfaces
+{
+    internal class IGroupRepository
+    {
+    }
+}
